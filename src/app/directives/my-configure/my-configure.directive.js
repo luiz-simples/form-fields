b31a8f8717a0b5752f18f12bb170ead90df22b10
Luiz Amorim <luiz.simples@gmail.com>
2014-11-14 22:11:06 -0200

    Op

--- src/app/directives/my-configure/my-configure.directive.js
+++ src/app/directives/my-configure/my-configure.directive.js
@@ -7,11 +7,36 @@ this.mySystem.directive('myConfigure', function MyItemDirective(itemTypes) {
     templateUrl: 'app/directives/my-configure/my-configure.html',
     link: function MyConfigureLink(scope, element) {
       scope.itemTypes = itemTypes;
+      var item;
 
       scope.$on('scanner-started', function(event, args) {
-        scope.myItemObj = _.cloneDeep(args.item);
+        item = args.item;
+        scope.myItemObj = _.cloneDeep(item);
+
+        if (!scope.myItemObj.cols)
+          scope.myItemObj.cols = [[]];
+
+        scope.myItemObj.columns = scope.myItemObj.cols.length;
         element.modal('show');
       });
+
+      scope.saveConfigItem = function SaveConfigItem() {
+        if (scope.myItemObj.type === 'container') {
+          while(scope.myItemObj.cols.length > scope.myItemObj.columns) {
+            var col = scope.myItemObj.pop();
+            var lastIndex = scope.myItemObj.cols.length-1;
+            scope.myItemObj.cols[lastIndex] = scope.myItemObj.cols[lastIndex].concat(col);
+          }
+
+          while(scope.myItemObj.cols.length < scope.myItemObj.columns)
+            scope.myItemObj.cols.push([]);
+
+            delete scope.myItemObj.columns;
+        }
+
+        for (var key in scope.myItemObj)
+          item[key] = scope.myItemObj[key];
+      };
     }
   };
 });
--- src/app/directives/my-item/my-item.directive.js
+++ src/app/directives/my-item/my-item.directive.js
@@ -1,11 +1,15 @@
 'use strict';
 
-this.mySystem.directive('myItem', function MyItemDirective($http, $templateCache, $compile) {
+this.mySystem.directive('myItem', function MyItemDirective($http, $templateCache, $compile, $rootScope) {
   return {
     restrict: 'AE',
     replace: true,
     scope: { myItem: '=' },
     link: function MyItemLink(scope, element) {
+      scope.editItem = function(item) {
+        $rootScope.$broadcast('scanner-started', { item: item });
+      };
+
       var template = 'my-item';
 
       if (scope.myItem.type === 'container')
